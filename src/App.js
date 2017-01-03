b57fcce8e0728ed5bffd1ee626f80c67ca4a0e9b
Roy M J <roymj88@gmail.com>
2017-01-03 19:06:14 +0530

    #3 - Defining prop in outer JSX

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
+    let txt = this.props.txt
     return (
         <div> 
-          <h1>{this.props.txt}</h1>
+          <h1>{txt}</h1>
         </div>
       )
   }
