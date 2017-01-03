610d0aefc37af3088361a30f1336423a268de39c
Roy M J <roymj88@gmail.com>
2017-01-03 13:24:02 +0530

    #2 - Class component and Stateless component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App = () => <h1>Hello World</h1>
+class App extends React.Component {
+  render(){
+    return <h1>Hello Eggheads!</h1>
+  }
+}
+
+// const App = () => <h1>Hello Stateless</h1>
+
 
 export default App
