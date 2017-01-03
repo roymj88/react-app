8f5148ce299d3f03445751fd069a3d4a19e7d82e
Roy M J <roymj88@gmail.com>
2017-01-03 19:15:30 +0530

    #3 - Defining default values for props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React from 'react';
 class App extends React.Component {
   render(){
     let txt = this.props.txt
+    let dummy = this.props.dummy
     return (
         <div> 
           <h1>{txt}</h1>
+          <h3>{dummy}</h3>
         </div>
       )
   }
@@ -15,6 +17,12 @@ class App extends React.Component {
 App.propTypes = {
   txt: React.PropTypes.string,
   cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  cat: 5,
+  txt: "This is the default value",
+  dummy: "This is the default text"
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App cat={2} txt="This is the prop value"/>,
+  <App cat={2} txt="This is the prop value" />,
   document.getElementById('root')
 );
