44df5c99809675232abbef60e37915a242860f16
Roy M J <roymj88@gmail.com>
2017-02-06 15:29:43 +0530

    Using states

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      txt: 'This is the state txt'
+    }
+  }
   render(){
-    let txt = this.props.txt
-    let dummy = this.props.dummy
     return (
         <div> 
-          <h1>{txt}</h1>
-          <h3>{dummy}</h3>
+          <h3>{this.state.txt}</h3>
         </div>
       )
   }
-}
-
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  cat: 5,
-  txt: "This is the default value",
-  dummy: "This is the default text"
 }
 
 export default App
